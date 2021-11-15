b9240fdf4aa677aec97b07df1722ed47212ee98e
guangmin Tang <tansar@126.com>
2021-11-15 16:45:26 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# 与目前比较流行的ORM框架性能测试对比
+
+
+## 总体测试结果
+
+|   插入记录数    |      hisql(耗时)      |      sqlsugar(耗时)       |      freesql(耗时)       |
+|--------------------|----------------------|----------------------|----------------------|
+| 10条       | `0.0270秒`                  |`0.1710秒`                  |`0.1981秒`                  |
+| 100条 | `0.0395秒`                  |`0.2017秒`                  |`0.2502秒`                  |
+| 1000条 | `0.2154秒`                  |`0.5106秒`                  |`0.9807秒`                  |
+| 10000条   | `1.8028秒`                  |`3.5510秒`                  |`8.0375秒`                  |
+| 100000条   | `16.9936秒`                  |`33.2747秒`                  |`77.9188秒`                  |
+
+
+从测试结果来看 HiSql的性能测试名列前茅
+
+
+
+
+## hisql sqlsugar freesql 插入10条数据测试
+<img src="TestProject/image/10.png" alt="hisql sqlsugar freesql 插入10条数据测试结果"/>
+
+## hisql sqlsugar freesql 插入100条数据测试
+<img src="TestProject/image/100.png" alt="hisql sqlsugar freesql 插入100条数据测试结果"/>
+
+
+## hisql sqlsugar freesql 插入1000条数据测试
+<img src="TestProject/image/1000.png" alt="hisql sqlsugar freesql 插入1000条数据测试结果"/>
+
+## hisql sqlsugar freesql 插入10000条数据测试
+<img src="TestProject/image/10000.png" alt="hisql sqlsugar freesql 插入10000条数据测试结果"/>
+
+## hisql sqlsugar freesql 插入100000条数据测试
+<img src="TestProject/image/100000.png" alt="hisql sqlsugar freesql 插入100000条数据测试结果"/>
---
+++ TestProject/Demo_Init.cs
@@ -0,0 +1,91 @@
+﻿using HiSql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SqlSugar;
+namespace TestProject
+{
+    public class Demo_Init
+    {
+        public static HiSqlClient GetSqlClient()
+        {
+
+            HiSqlClient sqlclient = new HiSqlClient(
+                     new HiSql.ConnectionConfig()
+                     {
+                         DbType = DBType.SqlServer,
+                         DbServer = "local-HoneBI",
+                         ConnectionString = "server=(local);uid=sa;pwd=Hone@123;database=HiSql;",//; MultipleActiveResultSets = true;
+                                                                                                 //User="tansar",//可以指定登陆用户的帐号
+
+                        
+
+                         Schema = "dbo",
+                         IsEncrypt = true,
+                         IsAutoClose = false,
+                         SqlExecTimeOut = 60000,
+
+                         AppEvents = new AopEvent()
+                         {
+                             OnDbDecryptEvent = (connstr) =>
+                             {
+                                 //解密连接字段
+                                 //Console.WriteLine($"数据库连接:{connstr}");
+
+                                 return connstr;
+                             },
+                             OnLogSqlExecuting = (sql, param) =>
+                             {
+                                 //sql执行前 日志记录 (异步)
+
+                                 //Console.WriteLine($"sql执行前记录{sql} time:{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss ffff")}");
+                             },
+                             OnLogSqlExecuted = (sql, param) =>
+                             {
+                                 //sql执行后 日志记录 (异步)
+                                 //Console.WriteLine($"sql执行后记录{sql} time:{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss ffff")}");
+                             },
+                             OnSqlError = (sqlEx) =>
+                             {
+                                 //sql执行错误后 日志记录 (异步)
+                                 Console.WriteLine(sqlEx.Message.ToString());
+                             },
+                             OnTimeOut = (int timer) =>
+                             {
+                                 //Console.WriteLine($"执行SQL语句超过[{timer.ToString()}]毫秒...");
+                             }
+                         }
+                     }
+                     );
+
+
+            //sqlclient.CodeFirst.InstallHisql();
+
+            return sqlclient;
+        }
+
+        public static SqlSugarClient GetSugarClient()
+        {
+            SqlSugarClient db = new SqlSugarClient(new SqlSugar.ConnectionConfig()
+            {
+                ConnectionString = "server=(local);uid=sa;pwd=Hone@123;database=HiSql;",//连接符字串
+                DbType = SqlSugar.DbType.SqlServer,
+                IsAutoCloseConnection = true
+            });
+            return db;
+        }
+
+
+        public static IFreeSql GetFreeSqlClient()
+        {
+
+            IFreeSql fsql = new FreeSql.FreeSqlBuilder()
+               .UseConnectionString(FreeSql.DataType.SqlServer, "server=(local);uid=sa;pwd=Hone@123;database=HiSql;")
+               .UseAutoSyncStructure(true) //自动同步实体结构到数据库
+               .Build();
+            return fsql;
+        }
+    }
+}
