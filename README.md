73e4fbb2cf8039c737ef3d357e914475779e0cb6
guangmin Tang <tansar@126.com>
2022-01-07 11:26:40 +0800

    性能测试对比

--- README.md
+++ README.md
@@ -5,28 +5,57 @@
 
 |   插入记录数    |      hisql(耗时)      |      sqlsugar(耗时)       |      freesql(耗时)       |
 |--------------------|----------------------|----------------------|----------------------|
-| 10条       | `0.0270秒`                  |`0.1710秒`                  |`0.1981秒`                  |
-| 100条 | `0.0395秒`                  |`0.2017秒`                  |`0.2502秒`                  |
-| 1000条 | `0.2154秒`                  |`0.5106秒`                  |`0.9807秒`                  |
-| 10000条   | `1.8028秒`                  |`3.5510秒`                  |`8.0375秒`                  |
-| 100000条   | `16.9936秒`                  |`33.2747秒`                  |`77.9188秒`                  |
+| 5条       | `0.0107秒`                  |`0.0312秒`                  |`0.02675秒`                  |
+| 10条       | `0.0111秒`                  |`0.0307秒`                  |`0.0271秒`                  |
+| 50条 | `0.0174秒`                  |`0.0364秒`                  |`0.0430秒`                  |
+| 100条 | `0.0281秒`                  |`0.0472秒`                  |`0.0922秒`                  |
+| 200条 | `0.0310秒`                  |`0.0584秒`                  |`0.2319秒`                  |
+| 500条 | `0.0712秒`                  |`0.1127秒`                  |`0.5996秒`                  |
+| 1000条 | `0.1112秒`                  |`0.1871秒`                  |`0.8145秒`                  |
+| 10000条   | `1.0440秒`                  |`1.6415秒`                  |`5.3671秒`                  |
+| 100000条   | `10.3279秒`                  |`15.8685秒`                  |`46.0261秒`                  |
 
 
-从测试结果来看 HiSql的性能测试名列前茅
 
-hisql源码下载
+### 分析
+1. 5条数据时 HiSql比SqlSugar快2.91倍 比freesql快2.5倍,从数据可以看得出在这个数据档位sqlsugar表现是慢的 freesql比sqlsugar快14%
+
+2. 10条数量测试三个ORM的线性变化值不大 依然是hisql最快 freesql次之,sqlsugar相对比freesql慢一点
+   
+3. 50条数据时 hisql比sqlsugar快2.09倍 比freesql快2.47倍 ，此时sqlsugar比freesql快18%,可以看出freesql在数量增长时他的性能也是呈下滑趋势
+4. 100条数据时 hisql比sqlsugar快1.67倍 比freesql快3.28倍，sqlsugar比freesql快1.95倍,sqlsugar与Hisql差距慢慢在缩小，但freesql与其它两个orm框架相比差距越来越大
+5. 当数据达到100000条时 hisql依然比sqlsugar保持在绝对性的优势
+
+
+从测试结果来看 HiSql的性能测试名列前茅 sqlsugar次之 freesql表现最差
+
+ [HiSql 源码(github) https://github.com/tansar/HiSql](https://github.com/tansar/HiSql)
 ```cmd
 git clone https://github.com/tansar/HiSql.git
 ```
 
+ [HiSql 源码(github) https://github.com/tansar/HiSqlTestDemo](https://github.com/tansar/HiSqlTestDemo)
+```shell
+git clone https://github.com/tansar/HiSqlTestDemo.git
+```
 
+
+
+## hisql sqlsugar freesql 插入5条数据测试
+<img src="TestProject/image/5.png" alt="hisql sqlsugar freesql 插入10条数据测试结果"/>
 
 ## hisql sqlsugar freesql 插入10条数据测试
 <img src="TestProject/image/10.png" alt="hisql sqlsugar freesql 插入10条数据测试结果"/>
 
+## hisql sqlsugar freesql 插入50条数据测试
+<img src="TestProject/image/50.png" alt="hisql sqlsugar freesql 插入10条数据测试结果"/>
+
+
 ## hisql sqlsugar freesql 插入100条数据测试
 <img src="TestProject/image/100.png" alt="hisql sqlsugar freesql 插入100条数据测试结果"/>
 
+## hisql sqlsugar freesql 插入100条数据测试
+<img src="TestProject/image/200.png" alt="hisql sqlsugar freesql 插入100条数据测试结果"/>
 
 ## hisql sqlsugar freesql 插入1000条数据测试
 <img src="TestProject/image/1000.png" alt="hisql sqlsugar freesql 插入1000条数据测试结果"/>
@@ -36,3 +65,8 @@ git clone https://github.com/tansar/HiSql.git
 
 ## hisql sqlsugar freesql 插入100000条数据测试
 <img src="TestProject/image/100000.png" alt="hisql sqlsugar freesql 插入100000条数据测试结果"/>
+
+
+
+
+后续请关注bulkcopy的性能测试对比
--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -10,56 +10,53 @@ namespace TestProject
     {
         static void Main(string[] args)
         {
+            
 
-
-            Console.WriteLine("启用");
-
-            var fullname = typeof(TestProject.Table.HTest01).Assembly.FullName;
-
-            //HiSql.WebUI.Program.Main(new string[] { });
-
-            //hisql连接
+            //hisql连接 请先配置好数据库连接
             HiSqlClient sqlClient = Demo_Init.GetSqlClient();
-            //hisql需要初始货安装
+            //hisql需要初始货安装 只需要执行一次
             sqlClient.CodeFirst.InstallHisql();
 
 
             //freesql连接
             IFreeSql freeClient = Demo_Init.GetFreeSqlClient();
-
+            
             //sqlsugar连接
             SqlSugarClient sugarClient = Demo_Init.GetSugarClient();
 
             int _count = 100000;
-            Tuple<HiTable, List<HiColumn>> tabomdel = sqlClient.Context.DMInitalize.BuildTabStru(typeof(Table.HTest01));
-            int v = (int)sqlClient.Context.DBO.ExecCommand(sqlClient.Context.DMTab.BuildTabCreateSql(tabomdel.Item1, tabomdel.Item2, true));
 
-
+            sqlClient.CodeFirst.CreateTable(typeof(Table.HTest01));
 
             Console.WriteLine("初始化hisql专用表成功!");
-            Tuple<HiTable, List<HiColumn>> tabomdel2 = sqlClient.Context.DMInitalize.BuildTabStru(typeof(Table.HTest02));
-            int v2 = (int)sqlClient.Context.DBO.ExecCommand(sqlClient.Context.DMTab.BuildTabCreateSql(tabomdel2.Item1, tabomdel2.Item2, true));
+
+            sqlClient.CodeFirst.CreateTable(typeof(Table.HTest02));
+           
+            
             Console.WriteLine("初始化sqlsugar专用表成功!");
 
-            Tuple<HiTable, List<HiColumn>> tabomdel3 = sqlClient.Context.DMInitalize.BuildTabStru(typeof(Table.HTest03));
-            int v3 = (int)sqlClient.Context.DBO.ExecCommand(sqlClient.Context.DMTab.BuildTabCreateSql(tabomdel3.Item1, tabomdel3.Item2, true));
+ 
+            sqlClient.CodeFirst.CreateTable(typeof(Table.HTest03));
             Console.WriteLine("初始化freesql专用表成功!");
 
 
-            Console.WriteLine($"测试场景  向表中插入{_count}条数据");
+            Console.WriteLine($"测试场景  向表中插入{_count}条数据,都用各个ORM常用的插入试(不用bulkcopy因为这个方法是底层库提供的)");
+            Console.WriteLine($"用常规数据插入最适应日常应用场景");
 
+            
 
-
-
+           
             List<object> lstobj = new List<object>();
             List<Table.HTest02> lstobj2 = new List<Table.HTest02>();
             List<Table.HTest03> lstobj3 = new List<Table.HTest03>();
             Random random = new Random();
+
+            //插入的参数值都随机产生 以免数据库执行相同SQL时会有缓存影响测试结果
             for (int i = 0; i < _count; i++)
             {
                 //hisql可以用实体类也可以用匿名类
-                lstobj.Add(new { SID = (i + 1), UName = $"hisql{i}", Age = 20 + (i % 50), Salary = 5000 + (i % 2000) + random.Next(10), Descript = $"hisql初始创建" });
-
+                lstobj.Add( new { SID=(i+1), UName=$"hisql{i}",Age=20+( i % 50), Salary=5000+(i%2000)+ random.Next(10), Descript=$"hisql初始创建" });
+                
                 //sqlsugar用匿句类报错用实体类
                 lstobj2.Add(new Table.HTest02 { SID = (i + 1), UName = $"sqlsugar{i}", Age = 20 + (i % 50), Salary = 5000 + (i % 2000) + random.Next(10), Descript = $"sqlsugar初始创建" });
                 lstobj3.Add(new Table.HTest03 { SID = (i + 1), UName = $"freesql{i}", Age = 20 + (i % 50), Salary = 5000 + (i % 2000) + random.Next(10), Descript = $"freesql初始创建" });
@@ -69,50 +66,55 @@ namespace TestProject
             sqlClient.TrunCate("HTest01").ExecCommand();
             sqlClient.TrunCate("HTest02").ExecCommand();
             sqlClient.TrunCate("HTest03").ExecCommand();
-            Console.WriteLine("------------------------------");
-            Console.WriteLine("----------HiSql 测试----------");
-            Console.WriteLine($"HiSql 预热...{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}");
 
-
-            var temp1 = sqlClient.Query("HTest01").Field("*").Take(1).Skip(1).ToDynamic();
-
-            Console.WriteLine($"HiSql  正在插入数据\t{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}");
             Stopwatch sw = new Stopwatch();
+
+            #region freesql
+            sw.Reset();
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("----------FreeSql 测试----------");
+            Console.WriteLine($"FreeSql 预热...{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}");
+            var temp3 = freeClient.Queryable<Table.HTest03>().Where(w => w.Age < 0).ToList();
+            Console.WriteLine($"FreeSql  正在插入数据\t{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}");
             sw.Start();
-
-            sqlClient.Insert("HTest01", lstobj).ExecCommand();
-
+            freeClient.Insert<Table.HTest03>(lstobj3).ExecuteAffrows();
             sw.Stop();
+            Console.WriteLine($"FreeSql 数据插入{_count}条 耗时{sw.Elapsed}秒");
+            sw.Reset();
+            #endregion
 
 
-            Console.WriteLine($"hisql 数据插入{_count}条 耗时{sw.Elapsed}秒");
+            #region sqlsugar
             sw.Reset();
             Console.WriteLine("------------------------------");
             Console.WriteLine("----------SqlSugar 测试----------");
             Console.WriteLine($"SqlSugar 预热...{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}");
             var temp2 = sugarClient.Queryable<Table.HTest02>("HTest02").Where(w => w.Age < 1).ToList();
-
             Console.WriteLine($"sqlsugar  正在插入数据\t{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}");
             sw.Start();
             sugarClient.Insertable(lstobj2).AS("HTest02").ExecuteCommand();
             sw.Stop();
-
             Console.WriteLine($"sqlsugar 数据插入{_count}条 耗时{sw.Elapsed}秒");
             sw.Reset();
-            Console.WriteLine("------------------------------");
-            Console.WriteLine("----------FreeSql 测试----------");
+            #endregion
 
-            Console.WriteLine($"FreeSql 预热...{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}");
-            var temp3 = freeClient.Queryable<Table.HTest03>().Where(w => w.Age < 0).ToList();
 
-            Console.WriteLine($"FreeSql  正在插入数据\t{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}");
-            sw.Start();
-            //sugarClient.Insertable(lstobj2).AS("HTest02").ExecuteCommand();
-            freeClient.Insert<Table.HTest03>(lstobj3).ExecuteAffrows();
-            sw.Stop();
 
-            Console.WriteLine($"FreeSql 数据插入{_count}条 耗时{sw.Elapsed}秒");
+
+            #region hisql
             sw.Reset();
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("----------HiSql 测试----------");
+            Console.WriteLine($"HiSql 预热...{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}");
+            var temp1 = sqlClient.Query("HTest01").Field("*").Take(1).Skip(1).ToDynamic();
+            Console.WriteLine($"HiSql  正在插入数据\t{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}");
+            sw.Start();
+            sqlClient.Insert("HTest01", lstobj).ExecCommand();
+            sw.Stop();
+            Console.WriteLine($"hisql 数据插入{_count}条 耗时{sw.Elapsed}秒");
+            sw.Reset();
+            #endregion
+
 
             var s = Console.ReadLine();
         }
